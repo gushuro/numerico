e7bcec90c1e47c49f30c9a66a75340852b93e292
gushuro <gushurovich@gmail.com>
2016-10-11 10:47:08 -0300

    tp1 listo

--- tpdiferencias3.m
+++ tpdiferencias3.m
@@ -1,12 +1,12 @@
-Uref = adi(1/10000,30);
+Uref = adi(1/10000,50);
 deltast = [1/100, 1/200, 1/300, 1/400, 1/500];
 y = zeros(size(deltast,1));
 el = 1
 for i = deltast
-    U = adi(i, 30);
+    U = adi(i, 50);
     y(el) = max(max(abs(Uref-U)));
     el = el+1
 end
-plot(log(deltast), log(y), '.');
+plot(log10(deltast), log10(y), '.');
 
 orden_empirico = log(y(2)/y(1)) / log(deltast(2)/deltast(1))
